689681de814e02c4660022d32f57d4a896f97926
Mahesh Haldar <haldar.mahesh@gmail.com>
2018-11-13 19:20:33 +0530

    Updates

---
+++ precache-manifest.93b0d136c1155b4dec24d9c259739d31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cef12589e6e2775ce429",
+    "url": "/static/js/main.cef12589.chunk.js"
+  },
+  {
+    "revision": "be11ca4053bbc83d9f71",
+    "url": "/static/js/1.be11ca40.chunk.js"
+  },
+  {
+    "revision": "cef12589e6e2775ce429",
+    "url": "/static/css/main.09eb4d5c.chunk.css"
+  },
+  {
+    "revision": "2d3b0aa0aba15d4bb2fa9634e34dadf3",
+    "url": "/index.html"
+  }
+];
