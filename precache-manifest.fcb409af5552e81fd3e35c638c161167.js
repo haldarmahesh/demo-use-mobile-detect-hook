45f4c0cddfe38e5ad67c246275e1962db8be8f43
Mahesh Haldar <haldar.mahesh@gmail.com>
2018-11-13 19:26:42 +0530

    Updates

---
+++ precache-manifest.fcb409af5552e81fd3e35c638c161167.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f95b47d6ae542a269f7e",
+    "url": "/demo-use-mobile-detect-hook/static/js/runtime~main.f95b47d6.js"
+  },
+  {
+    "revision": "5d21291cd744befc3455",
+    "url": "/demo-use-mobile-detect-hook/static/js/main.5d21291c.chunk.js"
+  },
+  {
+    "revision": "be11ca4053bbc83d9f71",
+    "url": "/demo-use-mobile-detect-hook/static/js/1.be11ca40.chunk.js"
+  },
+  {
+    "revision": "5d21291cd744befc3455",
+    "url": "/demo-use-mobile-detect-hook/static/css/main.09eb4d5c.chunk.css"
+  },
+  {
+    "revision": "d84b35513f37a7593af18e3a5d649eef",
+    "url": "/demo-use-mobile-detect-hook/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.93b0d136c1155b4dec24d9c259739d31.js"
+  "/demo-use-mobile-detect-hook/precache-manifest.fcb409af5552e81fd3e35c638c161167.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/demo-use-mobile-detect-hook/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
